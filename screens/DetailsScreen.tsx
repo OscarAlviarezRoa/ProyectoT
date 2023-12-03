4cf0a8f03166cdaaa4b4cec76826d47bcdf458d4
maria ramirez mendez <mariaramireeez2002@gmail.com>
2023-12-03 14:56:31 -0400

    feat: enceder y apagar dispositivos

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -2,10 +2,10 @@ import { View,Button,Text, Dimensions } from "react-native";
 import tw from 'twrnc';
 import { CustomCard } from "../components/CustomCard/CustomCard";
 import * as React from 'react';
-import { useEffect } from 'react';
-import { useState } from "react"; 
+import {useCallback, useEffect} from 'react';
+import { useState } from "react";
 import { firebaseConfig } from "../services/firebase";
-import { collection, getFirestore, addDoc, getDocs, query, orderBy, getDoc, doc } from 'firebase/firestore'
+import { collection, getFirestore, addDoc, getDocs, query, orderBy, getDoc, doc,setDoc } from 'firebase/firestore'
 import { initializeApp } from "firebase/app";
 import {
     LineChart,
@@ -14,7 +14,7 @@ import {
     ProgressChart,
     ContributionGraph,
     StackedBarChart
-  } from "react-native-chart-kit";
+} from "react-native-chart-kit";
 
 // Realtime Database
 
@@ -28,7 +28,7 @@ const appFirebase = initializeApp(firebaseConfig)
 //const database = getDatabase(appFirebase)
 const db = getFirestore(appFirebase)
 
-//Realtime Database 
+//Realtime Database
 
 // const starCountRef = ref(database, 'posts/' + postId + '/starCount');
 // onValue(starCountRef, (snapshot) => {
@@ -37,25 +37,25 @@ const db = getFirestore(appFirebase)
 // });
 
 //email
-import { useCookies } from "react-cookie"; 
+import { useCookies } from "react-cookie";
 
 
 
 function DetailsScreen({ navigation }) {
 
-  //email
+    //email
 
-  const handleEmail = () => {
-    fetch("http://localhost:3000/sendEmail",{
-      method:"POST",
-      mode: "no-cors",
-      headers: {
-        'Access-Control-Allow-Origin': '*',
+    const handleEmail = () => {
+        fetch("http://localhost:3000/sendEmail",{
+            method:"POST",
+            mode: "no-cors",
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            }
+        })
+
     }
-    })
-    
-  }
-    
+
     //Realtime Database
 
     const [todoData,setTodoData] = useState([]);
@@ -65,7 +65,7 @@ function DetailsScreen({ navigation }) {
         const citySnapshot = await getDocs(citiesCol);
         const cityList = citySnapshot.docs.map(doc => doc.data());
         setTodoData(cityList);
-         
+
 
 
     })
@@ -84,8 +84,8 @@ function DetailsScreen({ navigation }) {
         // console.log(newPosts);
         // console.log('error');
         // setTodoData(newPosts);
-    // })
-    calldb()
+        // })
+        calldb()
 
     },[])
     //Realtime Database
@@ -95,7 +95,7 @@ function DetailsScreen({ navigation }) {
 
 
 
-    const [lista,setLista] = useState<Array<{card:{}, graph:[]}>>([])
+    const [lista,setLista] = useState<Array<{card:{}, graph:[], dispositivoId:string | number}>>([])
 
     const [graphs,setGraphs] = useState([])
 
@@ -107,7 +107,7 @@ function DetailsScreen({ navigation }) {
     const query = collection(db, "a0");
 
     useEffect(()=>{
-        
+
         const getLista =async () => {
             try{
 
@@ -115,10 +115,10 @@ function DetailsScreen({ navigation }) {
 
                 const dispositivos = []
                 querySnapshot2.forEach((doc) => {
-                  if (doc.id!=="b0"){
-                    dispositivos.push(doc.id)
+                    if (doc.id!=="b0"){
+                        dispositivos.push(doc.id)
 
-                  }
+                    }
                 });
                 let resultadosLista = []
                 let graphValues= []
@@ -126,7 +126,7 @@ function DetailsScreen({ navigation }) {
                 dispositivos.map(async (dispositivo,index)=>{
                     try{
 
-                       const query =  await getDocs(collection(db, "a0", "b0",dispositivo))
+                        const query =  await getDocs(collection(db, "a0", "b0",dispositivo))
 
                         let docs = []
                         query.forEach((doc)=>{
@@ -156,7 +156,8 @@ function DetailsScreen({ navigation }) {
                         const sortValues2 = sortValues.sort((a,b)=> a.myDouble3-b.myDouble3)
                         resultadosLista.push({
                             card:resultado[0],
-                            graph: sortValues2
+                            graph: sortValues2,
+                            dispositivoId:dispositivo
                         })
                         setLista(resultadosLista)
 
@@ -174,117 +175,150 @@ function DetailsScreen({ navigation }) {
 
 
 
-                    
+
 
 
             }catch(error){
                 console.log(error)
             }
-            
+
         }
         setInterval(()=>getLista(),5000)
     },[])
     console.log(lista)
+    const OnButton = useCallback(async (dispositivo)=>{
+        await setDoc(doc(db, "b0",dispositivo), {
+            Sensor: 'A',
+        });
+    },[])
+    const OffButton = useCallback(async (dispositivo)=>{
+        await setDoc(doc(db, "b0",dispositivo), {
+            Sensor: 'B',
+        });
+    },[])
 
 
 
     return (
         <View style={tw`bg-red-50`}>
-      <View style={tw`flex flex-row flex-wrap justify-center items-center py-4`}>
-        {lista && lista.map(({card,graph},index)=><>
-            <CustomCard key={index} device={card.nombre} title='Lectura' date={new Date()} total={card.myDouble3} consumes={[
-            {
-                title:'Corriente',
-                readValue: card.IP,
-                isPositive: true,
-        
-            },
-            {
-                title:'Potencia',
-                readValue:card.IRMS,
-                isPositive: true,
-            },
-            {
-                title:'Consumo',
-                readValue:card.Potencia,
-                isPositive: null,
-            },
-            {
-                title:'Consumo Periodo',
-                readValue:card.myDouble3,
-                isPositive: null,
-            }
-        ]
+            <View style={tw`flex flex-row flex-wrap justify-center items-center py-4`}>
+                {lista && lista.map(({card,graph,dispositivoId},index)=><>
+                    <View style={tw`flex flex-row  justify-evenly py-2`}>
+                        <View style={tw`flex flex-col`}>
+                            <View style={tw`py-1`}>
+                                <Button  color="#F08080" title="Encender"
+                                         onPress = {()=>OnButton(dispositivoId)}
+                                />
+                            </View>
+
+                            <View style={tw`py-1`}>
+                                <Button color="#F08080" title="Apagar"
+                                        onPress = {()=>OffButton(dispositivoId)}
+                                />
+                            </View>
+
+                        </View>
+                        <View>
+                            <CustomCard key={index} device={card.nombre} title='Lectura' date={new Date()} total={card.myDouble3} consumes={[
+                                    {
+                                        title:'Corriente',
+                                        readValue: card.IP,
+                                        isPositive: true,
+
+                                    },
+                                    {
+                                        title:'Potencia',
+                                        readValue:card.IRMS,
+                                        isPositive: true,
+                                    },
+                                    {
+                                        title:'Consumo',
+                                        readValue:card.Potencia,
+                                        isPositive: null,
+                                    },
+                                    {
+                                        title:'Consumo Periodo',
+                                        readValue:card.myDouble3,
+                                        isPositive: null,
+                                    }
+                                ]
 
 
 
-        }/>
-            <LineChart
-                data={{
-                    labels: graph.map(item=>item.myDouble3),
-                    datasets: [
-                        {
-                            data: graph.map(item=>item.IRMS)
-                        }
-                    ]
-                }}
-                width={Dimensions.get("window").width} // from react-native
-                height={220}
-                yAxisLabel=""
-                yAxisSuffix="mA"
-                yAxisInterval={1} // optional, defaults to 1
-                chartConfig={{
-                    backgroundColor: "#F7F9F9",
-                    backgroundGradientFrom: "#F7F9F9",
-                    backgroundGradientTo: "#F7F9F9",
-                    decimalPlaces: 2, // optional, defaults to 2dp
-                    color: (opacity = 1) => "yellow",
-                    labelColor: (opacity = 1) => "black",
-                    style: {
-                        borderRadius: 16
-                    },
-                    propsForDots: {
-                        r: "6",
-                        strokeWidth: "2",
-                        stroke: "#ffa726"
-                    }
-                }}
-                bezier
-                style={{
-                    marginVertical: 8,
-                    borderRadius:16
-                }}
-            />
-        </>)}
 
-      </View>
-        <View>
-        <Text></Text>
+                        }/>
+                        </View>
 
-        <Button color="#F08080"
-            title="Estadisticas"
-            onPress={() => navigation.push('Details')} 
-          />
-          
-          <Button color="#F08080" title="Go to Home" onPress={() => navigation.navigate('Home') } />
-        </View>
-        {
-            todoData.map((item, index)=>{
-                return(
-                    <View key={index}>
-                        <Text>{item.nombre}</Text>
                     </View>
 
-                )
+                    <LineChart
+                        data={{
+                            labels: graph.map(item=>item.myDouble3),
+                            datasets: [
+                                {
+                                    data: graph.map(item=>item.IRMS)
+                                }
+                            ]
+                        }}
+                        width={Dimensions.get("window").width} // from react-native
+                        height={220}
+                        yAxisLabel=""
+                        yAxisSuffix="mA"
+                        yAxisInterval={1} // optional, defaults to 1
+                        chartConfig={{
+                            backgroundColor: "#F7F9F9",
+                            backgroundGradientFrom: "#F7F9F9",
+                            backgroundGradientTo: "#F7F9F9",
+                            decimalPlaces: 2, // optional, defaults to 2dp
+                            color: (opacity = 1) => "yellow",
+                            labelColor: (opacity = 1) => "black",
+                            style: {
+                                borderRadius: 16
+                            },
+                            propsForDots: {
+                                r: "6",
+                                strokeWidth: "2",
+                                stroke: "#ffa726"
+                            }
+                        }}
+                        bezier
+                        style={{
+                            marginVertical: 8,
+                            borderRadius:16
+                        }}
+                    />
 
-            })
-        }
-      </View>
+
+                </>)}
+
+            </View>
+            <View>
+                <Text></Text>
+
+                <Button color="#F08080"
+                        title="Estadisticas"
+                        onPress={() => navigation.push('Details')}
+                />
+
+                <Button color="#F08080" title="Go to Home" onPress={() => navigation.navigate('Home') } />
+            </View>
+            {
+                todoData.map((item, index)=>{
+                    return(
+                        <View key={index}>
+                            <Text>{item.nombre}</Text>
+                        </View>
+
+                    )
+
+                })
+            }
+        </View>
     );
-  }
+}
 
-    //lista
-   
-    //mostrar datos de los arreglos
+//lista
 
-  export default DetailsScreen;
+//mostrar datos de los arreglos
+
+export default DetailsScreen;
