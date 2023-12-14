0a1781de60e2f93436818e44431a029fd231dc0e
maria ramirez mendez <mariaramireeez2002@gmail.com>
2023-12-14 19:09:49 -0400

    feat: update device mobile alias

--- screens/DeviceScreen.tsx
+++ screens/DeviceScreen.tsx
@@ -62,8 +62,8 @@ function DeviceScreen({ navigation }) {
 
 
     const [inputAlias,setInputAlias ] = useState()
-    const handlerAlias = useCallback((event:ChangeEvent<HTMLInputElement>)=>{
-        setInputAlias(event.target.value)
+    const handlerAlias = useCallback((text)=>{
+        setInputAlias(text)
     },[])
 
 
@@ -244,7 +244,11 @@ function DeviceScreen({ navigation }) {
                             />
                             <View style={tw`flex flex-row justify-center`}>
                                 <View>
-                                    <TextInput onChange={handlerAlias} style={styles.input} />
+                                    <TextInput
+                                        onChangeText={handlerAlias}
+                                        value={inputAlias}
+                                        style={styles.input}
+                                    />
                                     <Button
                                         color="#7ACFFF"
                                         title='agregar alias al dispositivo'
